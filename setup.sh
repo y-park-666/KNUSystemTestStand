022a41ab9107fffc34bd9890305d710e2b6d225b
Taiwoo Kim <taiwoo.kim@cern.ch>
2024-08-07 17:05:31 +0900

    setup updated

--- setup.sh
+++ setup.sh
@@ -8,3 +8,13 @@ source program.sh
 # tamalero setup
 cd ../module_test_sw
 source setup.sh
+# controlhub run
+/opt/cactus/bin/controlhub_start
+PROMPT_COMMAND='echo -en "\033]0; Tamalero \a"'
+echo "\n"
+echo "Please turn on the readout board, using this command:"
+#echo "ipython3 test_tamalero.py -- --control_hub --kcu 192.168.0.10 --verbose --configuration modulev0b --power_up"
+echo "ipython3 test_tamalero.py -- --kcu 192.168.0.10 --verbose --configuration modulev0b --power_up"
+echo "\n"
+echo "And check if lpGBT gates are locked:"
+echo "ipython3 -i test_ETROC.py -- --test_chip --hard_reset --partial --configuration modulev0b --module 1"
